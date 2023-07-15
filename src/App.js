d011339466402455bf7e9153150802ebc098cfe0
Hemamalini-Kamaraj <hemamalini.ahkm@gmail.com>
2023-07-15 12:06:21 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import ReadNotes from "./components/ReadNotes";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [showStatus, setShowStatus] = useState("all");
+
+  const fetchAllNotes = async () => {
+    await axios
+      .get("https://hemamalini-noteapp.onrender.com/api/notes/")
+      .then((response) => setNotes(response.data));
+  };
+
+  useEffect(() => {
+    fetchAllNotes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/readNotes" style={{ paddingLeft: 15 }}>
+          Read Notes
+        </Link> 
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/readNotes"
+          element={
+            <ReadNotes
+              notes={notes}
+              showStatus={showStatus}
+              setShowStatus={setShowStatus}
+            />
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
